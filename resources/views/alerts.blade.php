1793e3bd49cd1e8795c6d56becbb08ae905ca46f
Steven Berg <steven@stevenberg.net>
2023-03-29 17:28:36 -0400

    Update alerts design

--- resources/views/alerts.blade.php
+++ resources/views/alerts.blade.php
@@ -1,10 +1,15 @@
 @if (App\status_message())
-  <p class="alert">
+  <p class="alert bg-blue color-blue">
     {{ App\status_message() }}
   </p>
 @endif
 @if (App\status_message('success'))
-  <p class="alert">
+  <p class="alert bg-blue color-blue">
     {{ App\status_message('success') }}
+  </p>
+@endif
+@if (App\status_message('failure'))
+  <p class="alert bg-red color-red">
+    {{ App\status_message('failure') }}
   </p>
 @endif
